8f98148cb56b80c796428b5e9396b96726ec8f83
Kim-kym <msgim725@gmail.com>
2024-12-22 14:15:53 +0900

    practice

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import hambuger from "./image/"
+
+function App() {
+  
+  return (
+    <div>
+    <button><img src={hambuger} alt='logo image'>hambuger</img></button>
+    </div>
+  )
+}
+
+
+export default App;
